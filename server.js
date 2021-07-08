f94e1adfa6b6f076e1a77b8c6d818079838a95dc
gjspencer22 <gjspencer22@gmail.com>
2021-07-07 21:56:45 -0600

    for tutor

--- server.js
+++ server.js
@@ -2,21 +2,30 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/notes');
+// const htmlRoutes = require('./routes/htmlRoutes');
 const fs = require('fs');
-const db = require('./db');
-
-// const app = express();
+const db = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+app.get("/", (req, res) => {
+    res.json(db.json);
+    
+})
+
+app.get("/notes", (req, res) => {
+    
+})
+
+
